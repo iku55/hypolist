8e67eb90e8098070997b915f63e605bcf10f009a
iku55 <ikscratch27@gmail.com>
2022-03-28 23:15:28 +0900

    追加:データ元attribution

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ var map = L.map('map', {
 var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 });
+map.attributionControl.addAttribution('データ: 気象庁 震源リスト');
 tileLayer.addTo(map);
 
 function changeTab(tab) {
